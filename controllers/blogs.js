86a8860b68c8d2e660317012db1dd06cfc980cd6
aishat001 <waheedaishat@gmail.com>
2021-06-30 12:40:01 +0100

    blog expansion

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 
 blogRouter.get('/',async (request, response) => {
@@ -8,10 +9,23 @@ blogRouter.get('/',async (request, response) => {
       response.json(blogs)
 })
 
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 blogRouter.post('/',async (request, response) => {
   const body = request.body
-
-  const user = await User.findById(body.userId)
+  
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
